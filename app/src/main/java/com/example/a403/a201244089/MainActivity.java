1f17d2d02040180e3f4784b6b9fd5acfab2d617b
LBH <dlqudgml0503@naver.com>
2016-11-11 12:13:41 +0900

    현재 별점 메세지 출력
    checkbox 체크 여부 메시지 출력.

--- app/src/main/java/com/example/a403/a201244089/MainActivity.java
+++ app/src/main/java/com/example/a403/a201244089/MainActivity.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button b1;
+    Button b2;
+    CheckBox ckb1;
+    RatingBar star1;
+    CalendarView calender1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"안녕?", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b2 = (Button)findViewById(R.id.button3);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"I`m fine", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ckb1=(CheckBox)findViewById(R.id.checkBox);
+        ckb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ckb1.isChecked()){
+                    Toast.makeText(getApplicationContext(),"체크 되었습니다.",Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(),"체크 해제 되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        star1=(RatingBar)findViewById(R.id.ratingBar);
+        star1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getApplicationContext(),"현재 별점은 "+rating+"입니다.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        calender1=(CalendarView) findViewById(R.id.calendarView);
+        calender1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
